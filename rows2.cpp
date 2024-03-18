c96f63226d0d4918b3cd80083be7af99878da175
TonyQ <150294902+tonyqua@users.noreply.github.com>
2024-03-18 20:40:27 +0200

    Add files via upload

---
+++ rows2.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Anton on 18.03.2024.
+//
+#include <stdio.h>
+
+int main() {
+    const int ROWS = 6;
+    const int CHARS = 6;
+
+    int row;
+    char ch;
+
+    for(row = 0; row < ROWS; row++){
+        for(ch = ('A' + row); ch < ('A' + CHARS); ch++){
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
